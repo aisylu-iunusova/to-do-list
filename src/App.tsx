3122adea467ae53a4439ce29dd8c4b30678e7ca0
Aisylu Iunusova <aisylu.iunusova@gmail.com>
2021-09-29 00:27:20 +0300

    Added saving of to-do list to localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./App.module.css";
 import TodoListItem, { Todo } from "./components/TodoListItem";
 import AddTodoForm, { AddTodo } from "./components/AddTodoForm";
 
-const initialTodos: Todo[] = [
-  {
-    text: "Cook the dinner",
-    complete: false,
-    date: "25.09.2021",
-    time: "7:42",
-  },
-  {
-    text: "Read the manga",
-    complete: true,
-    date: "8.14.2020",
-    time: "7:42",
-  },
-];
+// const initialTodos: Todo[] = [
+//   {
+//     text: "Cook the dinner",
+//     complete: false,
+//     date: "25.09.2021",
+//     time: "7:42",
+//   },
+//   {
+//     text: "Read the manga",
+//     complete: true,
+//     date: "8.14.2020",
+//     time: "7:42",
+//   },
+// ];
 
 const App = () => {
-  const [todos, setTodos] = useState(initialTodos);
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    let toDoList = localStorage.getItem("Todo") || [];
+    if (typeof toDoList === "string") {
+      setTodos(JSON.parse(toDoList));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("Todo", JSON.stringify(todos));
+    console.log(localStorage.getItem("Todo"));
+  }, [todos]);
 
   const toggleTodo = (selectedTodo: Todo) => {
     const newTodoList = todos.map((item) => {
@@ -36,23 +48,24 @@ const App = () => {
     if (text) {
       const date = new Date().toLocaleDateString();
       const time = new Date().toLocaleTimeString().slice(0, -3);
-      return setTodos([...todos, { text, complete: false, date, time }]);
+      setTodos([...todos, { text, complete: false, date, time }]);
     }
   };
 
   const removeTodo = (delTodo: Todo) => {
     todos.splice(todos.indexOf(delTodo), 1);
-    return setTodos(todos);
+    setTodos(todos);
   };
 
   return (
     <>
       <div className={styles.toDoList}>
-        {todos.map((item) => (
+        {todos.map((item, index) => (
           <TodoListItem
             todo={item}
             toggleTodo={toggleTodo}
             removeTodo={removeTodo}
+            key={index}
           />
         ))}
         <AddTodoForm addTodo={addTodo} />
